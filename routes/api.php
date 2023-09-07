e446917a85444dc2119aa1b6303beac222de4339
HeadStudios <kosta@headstudios.com.au>
2023-09-07 10:18:36 +1000

    Update api.php
    Fixing route problems

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,5 @@
 <?php
+use Illuminate\Support\Facades\Route;
 
 Route::get('/{resource}/attachable/{relationship}', '\NovaAttachMany\Http\Controllers\AttachController@create');
 Route::get('/{resource}/{resourceId}/attachable/{relationship}', '\NovaAttachMany\Http\Controllers\AttachController@edit');
